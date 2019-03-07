c072d999ba0b44bae07007abe9459e5b7bdedbed
Matthew Chesser <chesserm@umich.edu>
2019-03-07 14:42:15 -0500

    Completely updated rpn.py
    Rpn.py was never completed in lecture. So, my tests were failing due to
    incomplete code (not because Travis CI wasn't working). So, I updated
    rpn.py to be an exact copy of rpn.py found at
    https://gitlab.eecs.umich.edu/c4cs/rpn/blob/master/rpn.py

--- rpn.py
+++ rpn.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python3
 
+import operator
+
+operators = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+        '/': operator.truediv,
+}
+
 
 def calculate(arg):
-    stack = []
-    tokens = arg.split()
-    
+    stack = list()
+    for token in  arg.split():
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
 
 
 def main():
     while True:
-        calculate(input("rpn calc> "))
-
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
 
 # Main is run
 if __name__ == '__main__':
