3d446c8c33d1b5ebf26fd049814d117355b8e7f0
Matthew Chesser <chesserm@umich.edu>
2019-03-07 14:48:21 -0500

    Added failing tests for ^ operator
    Added two failing tests for the carrot operator (exponentiation).

--- test_rpn.py
+++ test_rpn.py
@@ -5,6 +5,10 @@ class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate('1 1 +')
         self.assertEqual(2, result)
+        result = rpn.calculate('2 2 ^')
+        self.assertEqual(4, result)
+        result = rpn.calculate('2 3 ^')
+        self.assertEqual(8, result)
 
 
 # TestBasics tb;
