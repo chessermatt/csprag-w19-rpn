1508c5a0a81e549d5b1ef327c0ab3decefd10d2e
Matthew Chesser <chesserm@umich.edu>
2019-03-07 14:07:34 -0500

    Pushing starting code to github remote
    Pushing starting code to the github remote repository. This should only have the rpn from lecture and the hw7 pdf instructions

---
+++ rpn.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+def calculate(arg):
+    stack = []
+    tokens = arg.split()
+    
+
+
+def main():
+    while True:
+        calculate(input("rpn calc> "))
+
+
+# Main is run
+if __name__ == '__main__':
+    main()
